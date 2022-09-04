27531f64f0fb0e4a91d80efed6c91bef42b54999
Sakhawat Hussain <sakhawathussainkaka@gmail.com>
2022-09-04 07:24:58 +0500

    Fixed error and edited gitignore

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Models\category;
+use App\Models\Category;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
